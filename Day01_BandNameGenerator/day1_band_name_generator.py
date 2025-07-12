7d4ca4588956765a27a6eb13f858f98410e22d78
Harshita Negi <harshitanegi015@gmail.com>
2025-07-13 00:00:06 +0530

    Add Day 1 Band Name Generator

---
+++ Day01_BandNameGenerator/day1_band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city_Name=input("What is the name of the city you grew up in ?\n")
+pet_Name=input("What is the name of your pet ?\n")
+band_name=print("Your Band Name could be "+ city_Name +" " + pet_Name)
